e32d0b9df98d8c934f4b273120a2826ad5562661
Whoopinstick <k.m.paul68@gmail.com>
2020-08-24 20:57:46 -0400

    Initial Commit

---
+++ 12 - Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  12 - Hello
+//
+//  Created by Kevin Paul on 8/24/20.
+//  Copyright © 2020 Whoopinstick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
